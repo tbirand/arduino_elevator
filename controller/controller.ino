44d4e6ddc334e7c395fb3ed5d3513554c9d9b5f6
Turgay Birand <tbirand@gmail.com>
2015-10-14 10:53:26 +0300

    Functions for lighting the 7 segment display. Count from 1 to 5 then 5 to 1 on push button test.

---
+++ controller/controller.ino
@@ -0,0 +1,248 @@
+#define A 7
+#define B 8
+#define C 2
+#define D 3
+#define E 4
+#define H 5
+#define G 6
+
+#define KAT1 9
+#define KAT2 10
+#define KAT3 11
+#define KAT4 12
+#define KAT5 13
+
+void clr()
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, HIGH);
+
+}
+void one()
+{
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(H, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+}
+
+void two()
+{
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+}
+
+void three()
+{
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+}
+
+void four()
+{
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(H, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+}
+
+void five()
+{
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+}
+
+void six()
+{
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+}
+
+void seven()
+{
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+}
+
+void eight()
+{
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+}
+
+void nine()
+{
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+}
+
+void zero()
+{
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(H, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+}
+
+void LoopDisplay()
+{
+  //  one();
+  //  delay(1000);
+  //  two();
+  //  delay(1000);
+  //  three();
+  //  delay(1000);
+  //  four();
+  //  delay(1000);
+  //  five();
+  //  delay(1000);
+  //  six();
+  //  delay(1000);
+  //  seven();
+  //  delay(1000);
+  //  eight();
+  //  delay(1000);
+  //  nine();
+  //  delay(1000);
+  //  zero();
+  //  delay(1000);
+}
+
+void setup()
+{
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(H, OUTPUT);
+  pinMode(G, OUTPUT);
+
+  pinMode(KAT1, INPUT);
+  pinMode(KAT2, INPUT);
+  pinMode(KAT3, INPUT);
+  pinMode(KAT4, INPUT);
+  pinMode(KAT5, INPUT);
+
+  goTo(5);
+
+  Serial.begin(9600);
+  Serial.println("Starting\n");
+}
+bool on = false;
+int currentFloor;
+
+void goTo(int floor) {
+  currentFloor = floor;
+  switch (floor) {
+    case 0:
+      zero();
+      break;
+    case 1:
+      one();
+      break;
+    case 2:
+      two();
+      break;
+    case 3:
+      three();
+      break;
+    case 4:
+      four();
+      break;
+    case 5:
+      five();
+      break;
+    case 6:
+      six();
+      break;
+    case 7:
+      seven();
+      break;
+    case 8:
+      eight();
+      break;
+    case 9:
+      nine();
+      break;
+  }
+}
+
+void loop()
+{
+  int test = digitalRead(KAT1);
+
+  if (test == HIGH && !on) {
+    on = true;
+    Serial.println("On");
+    if (currentFloor == 1)
+      for (int i = 1; i <= 5; i++) {
+        goTo(i);
+        delay(1000);
+      }
+    else {
+      for (int i = currentFloor; i >= 1; i--) {
+        goTo(i);
+        delay(1000);
+      }
+    }
+  }
+
+  if (test == LOW && on) {
+    on = false;
+    Serial.println("Off");
+  }
+
+  delay(100);
+
+  LoopDisplay();
+}
